56f9620415caf3dfd3aeb1717b9188b81e059576
devjojo <sa02045@naver.com>
2024-06-12 23:50:08 +0900

    feat: getAST function

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
+import fs from "node:fs";
+import { parse } from "@babel/parser";
+
+function getAST(filePath) {
+  const code = fs.readFileSync(filePath, "utf-8");
+  return parse(code, {
+    sourceType: "module",
+    plugins: ["jsx"],
+  });
+}
+
+const ALLOWED_EXTENSIONS = ["tsx", "jsx", "js", "ts"];
+
+function isAllowedFile(fileName) {
+  const [name, extension] = fileName.split(".");
+  return name === "page" && ALLOWED_EXTENSIONS.includes(extension);
+}
+
 const start = async () => {
-  console.log("Hello world");
+  const files = fs.readdirSync("./app");
+  files.forEach((file) => {
+    if (!isAllowedFile(file)) {
+      return;
+    }
+    const ast = getAST(`./app/${file}`);
+    console.log(ast);
+  });
 };
+
+start();
 
 export default start;
