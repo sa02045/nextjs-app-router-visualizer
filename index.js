0b09b9b0c10673ffd5598405b57744d9539dc5ec
devjojo <sa02045@naver.com>
2024-06-13 00:56:44 +0900

    feat: LinkComponent case

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import fs from "node:fs";
 import { parse } from "@babel/parser";
 import _traverse from "@babel/traverse";
 const traverse = _traverse.default;
+const APP_PATH = "./example/example/app/";
 
 function getAST(filePath) {
   const code = fs.readFileSync(filePath, "utf-8");
@@ -29,12 +30,12 @@ function isAllowedFile(fileName) {
 }
 
 const start = async () => {
-  const files = fs.readdirSync("./app");
+  const files = fs.readdirSync(APP_PATH);
   files.forEach((file) => {
     if (!isAllowedFile(file)) {
       return;
     }
-    const filePath = `./app/${file}`;
+    const filePath = APP_PATH + file;
     recursiveRouter(filePath);
   });
 
@@ -60,63 +61,89 @@ function isRouterPush(path) {
   );
 }
 
+function isLinkComponent(path) {
+  return (
+    path.isJSXElement() &&
+    path.get("openingElement").get("name").isJSXIdentifier({ name: "Link" })
+  );
+}
+
+function getEvent(path) {
+  let event;
+  while (path.parentPath) {
+    if (path.isJSXElement()) {
+      const openingElement = path.get("openingElement");
+      const attributes = openingElement.get("attributes");
+      const events = attributes.find(
+        (attr) =>
+          attr.get("name").isJSXIdentifier({ name: "onClick" }) ||
+          attr.get("name").isJSXIdentifier({ name: "onBlur" })
+      );
+      if (events) {
+        event = events.get("name").node.name;
+      }
+    }
+    path = path.parentPath;
+  }
+  return event;
+}
+
 function recursiveRouter(currentPath) {
   const currentAST = getAST(currentPath);
 
   traverse(currentAST, {
     enter(path) {
-      if (
-        path.isCallExpression() &&
-        path.get("callee").isMemberExpression() &&
-        path.get("callee.object").isIdentifier({ name: "router" }) &&
-        path.get("callee.property").isIdentifier({ name: "push" })
-      ) {
+      if (isRouterPush(path)) {
+        const event = getEvent(path);
         const args = path.get("arguments");
-        let event;
-        while (path.parentPath) {
-          if (path.isJSXElement()) {
-            const openingElement = path.get("openingElement");
-            const attributes = openingElement.get("attributes");
-            const events = attributes.find(
-              (attr) =>
-                attr.get("name").isJSXIdentifier({ name: "onClick" }) ||
-                attr.get("name").isJSXIdentifier({ name: "onBlur" })
-            );
-            if (events) {
-              event = events.get("name").node.name;
-            }
-          }
-          path = path.parentPath;
-        }
 
         if (args.length === 1) {
           const arg = args[0];
-          if (arg.isStringLiteral()) {
-            const nextPath = arg.node.value;
-            const nextFilePath = "./app" + nextPath + "/page.tsx";
-            const myCurrent = currentPath
-              .replace("./app", "")
-              .replace("/page.tsx", "");
-
-            if (isCyclic(myCurrent, nextPath)) {
-              return;
-            }
-
-            if (myCurrent === "") {
-              map.set("entry", {
-                path: nextPath,
-                event,
-              });
-            } else {
-              map.set(myCurrent, {
-                path: nextPath,
-                event,
-              });
-            }
-
-            recursiveRouter(nextFilePath);
+          if (!arg.isStringLiteral()) {
+            return;
           }
+          const nextPath = arg.node.value;
+          const nextFilePath = APP_PATH + nextPath + "/page.tsx";
+          const myCurrent = currentPath
+            .replace(APP_PATH, "")
+            .replace("page.tsx", "");
+
+          if (isCyclic(myCurrent, nextPath)) {
+            return;
+          }
+
+          map.set(myCurrent || "/", {
+            path: nextPath,
+            event,
+          });
+
+          recursiveRouter(nextFilePath);
         }
+      } else if (isLinkComponent(path)) {
+        const attributes = path.get("openingElement").get("attributes");
+        const href = attributes.find(
+          (attr) => attr.get("name").node.name === "href"
+        );
+
+        if (!href) {
+          return;
+        }
+        const nextPath = href.get("value").node.value;
+        const nextFilePath = APP_PATH + nextPath + "/page.tsx";
+        const myCurrent = currentPath
+          .replace(APP_PATH, "")
+          .replace("page.tsx", "");
+
+        if (isCyclic(myCurrent, nextPath)) {
+          return;
+        }
+
+        map.set(myCurrent || "/", {
+          path: nextPath,
+          event,
+        });
+
+        recursiveRouter(nextFilePath);
       }
     },
   });
