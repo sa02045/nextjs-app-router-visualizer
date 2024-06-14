cacef7748e78626470026ab093396beffc288abb
devjojo <sa02045@naver.com>
2024-06-14 22:04:39 +0900

    chore: remove unuse function

--- validNodePath.ts
+++ validNodePath.ts
@@ -1,7 +1,4 @@
-import _traverse, { type NodePath } from "@babel/traverse";
-
-// @ts-ignore
-const traverse = _traverse?.default as typeof _traverse;
+import { type NodePath } from "@babel/traverse";
 
 export function hasRouterPushJsxAttributeValue(path: NodePath) {
   let result = false;
@@ -43,34 +40,6 @@ export function hasRouterPush(path: NodePath) {
   return false;
 }
 
-export function isJsxArrowFunctionRouterPush(path: NodePath) {
-  let result = false;
-  if (path.isJSXOpeningElement()) {
-    path.traverse({
-      enter(childPath) {
-        if (hasRouterPush(childPath)) {
-          result = true;
-        }
-      },
-    });
-  }
-  return result;
-}
-
-function isFunctionDeclarationRouterPush(path: NodePath) {
-  if (path.isFunctionDeclaration()) {
-    const blockStatement = path.get("body");
-    blockStatement.traverse({
-      enter(path) {
-        if (hasRouterPush(path)) {
-          return true;
-        }
-      },
-    });
-  }
-  return false;
-}
-
 export function getPathName(routerPath: NodePath) {
   if (!routerPath.isCallExpression()) {
     return "";
@@ -102,107 +71,6 @@ export function getPathName(routerPath: NodePath) {
   }
 
   return "";
-}
-
-export function getJsxArrowFunctionTrigger(path: NodePath) {
-  path.assertJSXOpeningElement();
-  let trigger = "";
-
-  const attributes = path.get("attributes");
-
-  attributes.forEach((attribute) => {
-    if (attribute.isJSXAttribute()) {
-      const value = attribute.get("value");
-      console.log(value);
-    }
-  });
-  return trigger;
-}
-
-export function getTrigger(routerPath: NodePath, componentName: string) {
-  if (!routerPath.isCallExpression()) {
-    return "";
-  }
-
-  let trigger = "";
-
-  routerPath.findParent((parentPath) => {
-    if (parentPath.isFunctionDeclaration()) {
-      parentPath.traverse({
-        enter(childPath) {
-          if (hasRouterPush(childPath)) {
-            if (componentName !== parentPath.node.id?.name) {
-              trigger = parentPath.node.id?.name;
-            }
-          }
-        },
-      });
-      return true;
-    }
-    return false;
-  });
-
-  return trigger;
-}
-
-export function isArrowFunctionRouterPush(path: NodePath) {
-  let result = false;
-  let trigger = "";
-  let nextPath = "";
-
-  if (path.isArrowFunctionExpression()) {
-    const blockStatement = path.get("body");
-    blockStatement.traverse({
-      enter(path) {
-        if (
-          path.isCallExpression() &&
-          path.get("callee").isMemberExpression() &&
-          path
-            .get("callee.object")
-            .find((p) => p.isIdentifier({ name: "router" })) &&
-          path
-            .get("callee.property")
-            .find((p) => p.isIdentifier({ name: "push" }))
-        ) {
-          result = true;
-
-          const pushArguments = path.get("arguments");
-          const nextPathArg = pushArguments.find((arg) => {
-            if (arg.isStringLiteral() && arg?.node.value) {
-              return true;
-            }
-            return false;
-          });
-
-          if (nextPathArg?.isStringLiteral()) {
-            nextPath = nextPathArg.node.value;
-          }
-
-          const handler = findHandlerNameTraverseParent(path);
-          if (handler) {
-            trigger = handler;
-          }
-        }
-      },
-    });
-  }
-  return {
-    result,
-    nextPath,
-    trigger,
-  };
-}
-
-function findHandlerNameTraverseParent(path: NodePath) {
-  let handlerName = "";
-  path.findParent((p) => {
-    if (p.isJSXIdentifier() && p.node.name.startsWith("on")) {
-      handlerName = p.node.name;
-      return true;
-    }
-    return false;
-  });
-  return handlerName;
 }
 
 export function isLinkComponent(path: NodePath) {
