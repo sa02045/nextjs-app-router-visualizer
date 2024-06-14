0e9d37409934cddad003e18da2c80479677f5f31
sa02045 <sa02045@naver.com>
2024-06-14 18:15:01 +0900

    feat: ast function

---
+++ ast.ts
@@ -0,0 +1,10 @@
+import { parse } from "@babel/parser";
+import fs from "node:fs";
+
+export function getJsxAST(path: string) {
+  const code = fs.readFileSync(path, "utf-8");
+  return parse(code, {
+    sourceType: "module",
+    plugins: ["jsx"],
+  });
+}
