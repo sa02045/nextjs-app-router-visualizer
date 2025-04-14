f5b8c9c791e3f693013a760e288555b015365d94
seunghee <sa02045@naver.com>
2025-04-14 22:13:28 +0900

    chore: build folder (#4)

---
+++ src/bin.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+import cac from 'cac';
+import { start } from './index.js';
+
+const cli = cac('nextjs-app-router-visualizer');
+
+cli.help();
+
+cli.option('--entry -e <filepath>', 'Entry page file path');
+
+const parsed = cli.parse();
+
+const { entry, e } = parsed.options;
+
+start({
+  entryPagePath: entry || e,
+});
