85b78639ab6c16dc78433f3a12b6770c3926a023
devjojo <sa02045@naver.com>
2025-04-14 22:13:02 +0900

    chore: build folder

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
-export default defineConfig((options) => {
+export default defineConfig(options => {
   return {
-    entryPoints: ["src/index.ts"],
-    outDir: "./",
-    format: ["esm"],
+    entryPoints: ['src/index.ts', 'src/bin.ts'],
+    outDir: 'dist',
+    format: ['esm'],
   };
 });
