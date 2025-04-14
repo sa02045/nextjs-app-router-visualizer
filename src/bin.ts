a64d1c92bd4df91cc780948fd177c0556b5e611c
seunghee <sa02045@naver.com>
2025-04-14 22:35:26 +0900

    fix: path (#7)

--- src/bin.ts
+++ src/bin.ts
@@ -2,17 +2,43 @@
 
 import cac from 'cac';
 import { start } from './index.js';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import fs from 'node:fs';
 
 const cli = cac('nextjs-app-router-visualizer');
 
 cli.help();
-
-cli.option('--entry -e <filepath>', 'Entry page file path');
+cli.option('--entry <filepath>', 'Entry page file path');
 
 const parsed = cli.parse();
 
-const { entry, e } = parsed.options;
+const entryFromArgs = parsed.options.entry;
+
+let entryPagePath;
+
+if (entryFromArgs) {
+  entryPagePath = path.resolve(process.cwd(), entryFromArgs);
+} else {
+  const cwdAppPath = path.join(process.cwd(), 'app', 'page.tsx');
+  const cwdSrcAppPath = path.join(process.cwd(), 'src', 'app', 'page.tsx');
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const scriptRelativePath = path.join(__dirname, '..', 'app', 'page.tsx');
+
+  if (fs.existsSync(cwdAppPath)) {
+    entryPagePath = cwdAppPath;
+  } else if (fs.existsSync(cwdSrcAppPath)) {
+    entryPagePath = cwdSrcAppPath;
+  } else if (fs.existsSync(scriptRelativePath)) {
+    entryPagePath = scriptRelativePath;
+  }
+}
+
+console.log('Using entry page path:', entryPagePath);
 
 start({
-  entryPagePath: entry || e,
+  entryPagePath,
+  output: 'graph.html',
 });
